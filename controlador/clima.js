e383ae63b1a639346b36b45368e40e9fdf8faa9c
Herig Recalde <herig14@gmail.com>
2020-06-16 15:49:57 -0500

    Actualización

--- controlador/clima.js
+++ controlador/clima.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 const getClima = async(ciudad) => {
+    //codificación uri
     const ciudadURL = encodeURI(ciudad)
+        //Petición http get con axios
     const resp = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadURL}&appid=0dd168334f6427dfb96cc27425c390a3&units=metric`)
     return resp
 }
